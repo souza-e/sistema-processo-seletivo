d20959bff3b21f0dbed62ce608b1bfd5abe328ae
Eduardo <souza-e@hotmail.com>
2025-06-06 15:01:11 -0300

    feat. sistema de seleção de candidato e imprimir candidato feito

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,21 +1,67 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
 
         System.out.println("Bem-vindo ao Processo Seletivo!");
 
-        analisaCandidato(2000); // Chamada do método com o salário pretendido
-        analisaCandidato(2500); // Chamada do método com outro salário pretendido
-        analisaCandidato(1500); // Chamada do método com outro salário pretendido
+        imprimirCandidatos();
+
     }
 
-    static void analisaCandidato (double salarioPretendido) {
-       
+ static void imprimirCandidatos() {
+
+        String[] candidatos = {"João", "Maria", "Pedro", "Ana", "Lucas"};
+
+        System.out.println("Lista de Candidatos:");
+        for (int indice = 0; indice < candidatos.length; indice++) {
+          
+            System.out.println("O candidato de nº"+ (indice + 1) + " é: " + candidatos[indice]);
+        }
+    }
+
+
+    static void selecaoCandidato() {
+
+        String[] candidatos = {"João", "Maria", "Pedro", "Ana", "Lucas"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
         double salarioBase = 2000; // Salário base para análise, pode ser alterado conforme necessário
-       // salarioPretendido = 2000; // Exemplo de salário pretendido, pode ser alterado conforme necessário
-     
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("Candidato: " + candidato + ", Salário Pretendido: " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("Candidato " + candidato + " aprovado!");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+
+        }
+
+    }
+
+    static double valorPretendido() {
+
+        return ThreadLocalRandom.current().nextDouble(1800, 2200); // Gera um salário pretendido aleatório entre 1500 e 3000
+    }
+
+    
+   // ==========================================================
+    
+    static void analisaCandidato(double salarioPretendido) {
+
+        double salarioBase = 2000; // Salário base para análise, pode ser alterado conforme necessário
+        // salarioPretendido = 2000; // Exemplo de salário pretendido, pode ser alterado conforme necessário
+
         if (salarioPretendido <= salarioBase) {
             System.out.println("Candidato aprovado!");
         } else {
